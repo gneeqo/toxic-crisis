f007e1172d0c5ee608cde96e4cd24bf9948f4cb1
gneeqo <19242213+gneeqo@users.noreply.github.com>
2025-08-18 16:58:22 -0700

    camera and controller

--- Character/player.gd
+++ Character/player.gd
@@ -1,8 +1,9 @@
 class_name Player extends CharacterBody3D
 
-
+@export var Camera:Camera3D
 @export var movement_multiplier:float
-
+@export var mouse_sens:float
+var pitch:float = 0.0
 var server_controlled:bool
 var relativeMouseMotion:Vector2
 var client_controlled:bool
@@ -13,12 +14,12 @@ func _enter_tree() -> void:
     if name == "Player1":
         set_multiplayer_authority(NetworkManager.server_unique_id) 
         if NetworkManager.is_server:
-            $Camera3D.make_current()
+            Camera.make_current()
             my_player = true
     else:
         set_multiplayer_authority(NetworkManager.client_unique_id) 
         if not NetworkManager.is_server:
-            $Camera3D.make_current()
+            Camera.make_current()
             my_player = true
     
     $MultiplayerSynchronizer.root_path = get_path()
@@ -27,21 +28,25 @@ func _enter_tree() -> void:
 
 func _input(event:InputEvent)->void:
     if event is InputEventMouseMotion:
-        relativeMouseMotion = event.relative
+        if event.relative.is_zero_approx() == false:
+            rotate_y(deg_to_rad(-event.relative.x * mouse_sens))
+            pitch -= event.relative.y * mouse_sens
+            pitch = clamp(pitch,-90,90)
+            Camera.rotation_degrees.x = pitch
+             
         
         
     
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
     if not is_multiplayer_authority():
         return
+    var input_dir:Vector2 = Input.get_vector("strafe_left","strafe_right","forward","backward")
     
-    transform.basis = transform.basis.rotated(transform.basis.y.normalized(),-relativeMouseMotion.x / 500)
-    transform.basis = transform.basis.rotated(transform.basis.x.normalized(),-relativeMouseMotion.y / 500)
-    if Input.is_action_pressed("forward"):
-        var movement_vector:Vector3 = rotation.normalized()
-        velocity -= movement_vector * movement_multiplier
-        move_and_slide()
-
+    var strafe_direction:Vector3 = transform.basis.x * input_dir.x
+    var forward_direction:Vector3 = Camera.global_transform.basis.z * input_dir.y
     
+    velocity.x = (strafe_direction.x + forward_direction.x) * movement_multiplier
+    velocity.z = (strafe_direction.z + forward_direction.z) * movement_multiplier
+    move_and_slide()
 
     
